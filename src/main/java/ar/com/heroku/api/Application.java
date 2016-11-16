1de4fe798da485004a9b0466fb868d35616705f7
ariel.duarte <arielpy@gmail.com>
2016-11-16 03:13:11 -0300

    Added a enabling Cross Origin Resource Sharing for Spring Data Rest

--- src/main/java/ar/com/heroku/api/Application.java
+++ src/main/java/ar/com/heroku/api/Application.java
@@ -7,11 +7,16 @@ package ar.com.heroku.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @ComponentScan
@@ -23,5 +28,23 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);			
 	}
-
+	
+	/**
+	 * This method solve the issue about Spring Data Rest and Cors
+	 * No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'http://localhost:3000' is therefore not allowed access.
+	 */
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*"); //webapp name
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(0);
+		return bean;
+	}
 }
+
